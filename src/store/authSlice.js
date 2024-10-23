dd8d6eb57b6f51b9751d0b808dc87ddbc6db957c
Edwin <edwincastaneda0801@gmail.com>
2024-10-23 12:36:27 -0500

    auth slice

---
+++ src/store/authSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+import api from "./api";
+import { logout } from "../../../../2408.BookBuddy/src/features/account/authSlice";
+
+/** User create and log in acount endpoints*/
+
+const authApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (credentials) => ({
+        url: "/register",
+        method: "POST",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data.message,
+      invalidatesTags: ["User"],
+    }),
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/users/login",
+        method: "POST",
+        body: credentials,
+      }),
+      transformErrorResponse: (response) => response.data.message,
+      invalidatesTags: ["User"],
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useLoginMutation } = authApi;
+
+const TOKEN_KEY = "token";
+
+//Stores tha token in state and session storage
+const storeToken = (state, { payload }) => {
+  state.token = payload.token;
+  sessionStorage.setItem(TOKEN_KEY, payload.token);
+};
+
+// This slice keeps the JWT sent from the backend
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    token: sessionStorage.getItem(TOKEN_KEY),
+  },
+  reducers: {
+    logout: (state) => {
+      state.token = null;
+      sessionStorage.removeItem(TOKEN_KEY);
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(api.endpoints.login.matchFulfilled, storeToken);
+    builder.addMatcher(api.endpoints.register.matchFulfilled, storeToken);
+  },
+});
+
+export const { logout } = authSlice.actions;
+
+export const selectToken = (state) => state.auth.token;
+export default authSlice.reducer;
