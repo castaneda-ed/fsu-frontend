fd1dbf58974c7b55a72a1066854f0b5bce688f94
Edwin <edwincastaneda0801@gmail.com>
2024-10-23 11:21:22 -0500

    department slice endpoint done

--- src/store/departmentSlice.js
+++ src/store/departmentSlice.js
@@ -6,9 +6,50 @@ const departmentApi = api.injectEndpoints({
       query: () => "departments",
       transformResponse: (response) => response.departments,
       transformErrorResponse: (response) => response.departments.error,
-      providesTags: ["Department"],
+      invalidatesTags: ["Department"],
+    }),
+    getDepartment: build.query({
+      query: (id) => `departments/${id}`,
+      transformResponse: (response) => response,
+      transformErrorResponse: (response) => response.error,
+      invalidatesTags: ["Department"],
+    }),
+    postDepartment: build.mutation({
+      query: (newDepartment) => ({
+        url: "departments",
+        method: "POST",
+        body: newDepartment,
+      }),
+      transformResponse: (response) => response.department,
+      transformErrorResponse: (response) => response.error,
+      invalidatesTags: ["Department"],
+    }),
+    deleteDepartment: build.mutation({
+      query: (id) => ({
+        url: `departments/${id}`,
+        method: "DELETE",
+      }),
+      transformResponse: (response) => response.message,
+      transformErrorResponse: (response) => response.error,
+      invalidatesTags: ["Department"],
+    }),
+    updateDepartment: build.mutation({
+      query: (id, ...updatedDepartment) => ({
+        url: `departments/${id}`,
+        method: "PATCH",
+        body: updatedDepartment,
+      }),
+      transformResponse: (response) => response.updateDepartment,
+      transformErrorResponse: (response) => response.error,
+      invalidatesTags: ["Department"],
     }),
   }),
 });
 
-export const { useGetDepartmentsQuery } = departmentApi;
+export const {
+  useGetDepartmentsQuery,
+  useGetDepartmentQuery,
+  usePostDepartmentMutation,
+  useDeleteDepartmentMutation,
+  useUpdateDepartmentMutation,
+} = departmentApi;
