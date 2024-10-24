b7759e4643b3ff3f01a04de4bff7673d6e5f48ee
David <davidopotowsky14@gmail.com>
2024-10-24 12:06:40 -0500

    added dept name to prof details

--- src/features/ProfessorDetails.jsx
+++ src/features/ProfessorDetails.jsx
@@ -2,51 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetProfessorQuery } from '../store/facultySlice';
 
-// Fake data for professors 
-//keeping the below fake data 
-//for record's sake, can delete or change later
-const fakeProfessors = [
-  {
-    id: 1,
-    name: 'Mr. Lang',
-    bio: 'studied at FSU, class of 2020.',
-    profileImage: 'pic here',
-    contactInfo: '555-555-5555; lang@FSU.org',
-    departmentId: 1
-  },
-  {
-    id: 2,
-    name: 'Mrs. Reik',
-    bio: 'studied at TSU, class of 2006.',
-    profileImage: 'pic here',
-    contactInfo:'555-555-5552; reik@FSU.org',
-    departmentId: 2
-  },
-  {
-    id: 3,
-    name: 'Sir Yventek',
-    bio: 'studied at WSU, class of 2012.',
-    profileImage: 'pic here',
-    contactInfo: '555-555-5522; yventek@FSU.org',
-    departmentId: 3
-  },
-  {
-    id: 4,
-    name: 'Mme Gobstunt',
-    bio: 'studied at YSU, class of 2016',
-    profileImage: 'pic here',
-    contactInfo: '555-555-5222; gobstunt@FSU.org',
-    departmentId: 2
-  },
-  {
-    id: 5,
-    name: 'Dr. Stryknein',
-    bio: 'studied at HSU, class of 2011',
-    profileImage: 'pic here',
-    contactInfo: '555-555-2222; stryknein@FSU.org',
-    departmentId: 3
-  }
-];
+
 
 // export default function Professor() {
 const Professors = () => {
@@ -64,7 +20,7 @@ if (isLoading) return <p>Loading...</p>;
             <p>{professor.bio}</p>
             <img src={professor.image} alt={professor.name}/>
             <p>{professor.email}</p>
-            <p>{professor.departmentId}</p>
+            <p>{professor.department.name}</p>
           </li>
               </ul>
     </>
--- src/router.jsx
+++ src/router.jsx
@@ -5,7 +5,7 @@ import AuthForm from "./features/account/Authform";
 import DepartmentList from "./features/departments/DepartmentList";
 import DepartmentDetails from "./features/departments/DepartmentDetails";
 import ProfessorList from "./features/faculty/ProfessorList";
-import ProfessorDetails from "./features/faculty/ProfessorDetails";
+import ProfessorDetails from "./features/ProfessorDetails";
 
 const router = createBrowserRouter([
   {
@@ -14,10 +14,10 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <DepartmentList /> },
       { path: "/departments", element: <DepartmentList /> },
-      { path: "/departments/id", element: <DepartmentDetails /> },
+      { path: "/departments/:id", element: <DepartmentDetails /> },
       { path: "/login", element: <AuthForm /> },
       { path: "/faculty", element: <ProfessorList /> },
-      { path: "/faculty/id", element: <ProfessorDetails /> },
+      { path: "/faculty/:id", element: <ProfessorDetails /> },
     ],
   },
 ]);
